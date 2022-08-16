8976de8ce75c5ed2f0a68d7e89b6d96b17f35448
Matheus Abreu e Lima <abreuelima.matheus@gmail.com>
2022-08-16 17:20:36 -0300

    Tela inicial finalizada

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, library_private_types_in_public_api, use_key_in_widget_constructors
+
+import 'package:app_tc/ui/pages/search.dart';
 import 'package:flutter/material.dart';
-import 'package:app_tc/ui/home/home.dart';
+
+import 'ui/home/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +15,98 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Home(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _indiceAtual = 0;
+  List<IconData> icons = [
+    Icons.home,
+    Icons.person_search,
+    Icons.person,
+    Icons.chat_bubble,
+    Icons.settings
+  ];
+  List<IconData> iconsClicked = [
+    Icons.home_outlined,
+    Icons.person_search_outlined,
+    Icons.person_outline,
+    Icons.chat_bubble_outline,
+    Icons.settings_outlined
+  ];
+  final List<Widget> _telas = [
+    Home(),
+    SearchPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      body: _telas[_indiceAtual],
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.fromLTRB(40, 0, 40, 20),
+        child: Material(
+          borderRadius: BorderRadius.circular(20),
+          color: Color.fromARGB(255, 255, 255, 255),
+          child: SizedBox(
+            height: 50,
+            width: double.infinity,
+            child: ListView.builder(
+              itemCount: icons.length,
+              padding: EdgeInsets.symmetric(horizontal: 1.5),
+              itemBuilder: (ctx, i) => Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _indiceAtual = i;
+                    });
+                  },
+                  child: AnimatedContainer(
+                    duration: Duration(milliseconds: 250),
+                    width: 35,
+                    decoration: BoxDecoration(
+                      border: i == _indiceAtual
+                          ? Border(
+                              top: BorderSide(
+                                  width: 3.0,
+                                  color: Color.fromRGBO(38, 38, 38, 1)))
+                          : null,
+                      color: i == _indiceAtual
+                          ? Color.fromRGBO(217, 217, 217, 1)
+                          : null,
+                    ),
+                    child: Icon(
+                      i == _indiceAtual ? iconsClicked[i] : icons[i],
+                      size: 30,
+                      color: i == _indiceAtual
+                          ? Color.fromRGBO(38, 38, 38, 1)
+                          : Color.fromRGBO(38, 38, 38, 1),
+                    ),
+                  ),
+                ),
+              ),
+              scrollDirection: Axis.horizontal,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _indiceAtual = index;
+    });
   }
 }
--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unnecessary_const, prefer_const_constructors
+// ignore_for_file: unnecessary_const, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -8,11 +8,11 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(0, 38, 38, 38),
+        backgroundColor: Color.fromRGBO(38, 38, 38, 1),
         body: ListView(children: [
           Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * 0.4,
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height * 0.5,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(0),
@@ -20,96 +20,241 @@ class Home extends StatelessWidget {
                 bottomLeft: Radius.circular(25),
                 bottomRight: Radius.circular(25),
               ),
+              boxShadow: [
+                BoxShadow(
+                  color: Color.fromARGB(48, 0, 0, 0),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3), // changes position of shadow
+                ),
+              ],
               image: DecorationImage(
-                  image: AssetImage('assets/images/img_main.jpeg'),
-                  fit: BoxFit.cover,
-                  alignment: Alignment.center,
-                  repeat: ImageRepeat.noRepeat),
+                image: AssetImage('assets/images/img_main_text.png'),
+                fit: BoxFit.cover,
+                alignment: Alignment.center,
+                repeat: ImageRepeat.noRepeat,
+              ),
+            ),
+            alignment: Alignment.bottomCenter,
+            padding: EdgeInsets.fromLTRB(30, 0, 30, 10),
+            child: Text(
+              'Pronto para fazer a sua tatuagem? Clique aqui e encontre os profissionais disponíveis em sua região!',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+                fontFamily: 'Ubuntu',
+                fontSize: 17,
+                shadows: [
+                  Shadow(
+                    blurRadius: 15.0,
+                    color: Color.fromRGBO(53, 52, 52, 1),
+                    offset: Offset(3.0, 3.0),
+                  ),
+                ],
+              ),
+              textAlign: TextAlign.center,
             ),
           ),
           Padding(
             padding: const EdgeInsets.fromLTRB(10, 40, 10, 10),
             child: Container(
                 width: MediaQuery.of(context).size.width,
-                alignment: Alignment.bottomCenter,
+                alignment: Alignment.center,
                 child: const Text('✨ ENCONTRE INSPIRAÇÃO ✨',
                     style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20))),
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700,
+                      fontFamily: 'Ubuntu',
+                      fontSize: 22,
+                      shadows: [
+                        Shadow(
+                          blurRadius: 10.0,
+                          color: Color.fromARGB(54, 0, 0, 0),
+                          offset: Offset(3.0, 3.0),
+                        ),
+                      ],
+                    ))),
           ),
           GridView.count(
               crossAxisCount: 2,
               childAspectRatio: MediaQuery.of(context).size.width /
-                  (MediaQuery.of(context).size.height / 4),
+                  (MediaQuery.of(context).size.height / 3.5),
               shrinkWrap: true,
               scrollDirection: Axis.vertical,
               children: [
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Container(
-                      width: MediaQuery.of(context).size.width,
                       decoration: BoxDecoration(
-                        color: Colors.yellow,
                         borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/img_cat1.png'),
+                            fit: BoxFit.cover,
+                            alignment: Alignment.center,
+                            repeat: ImageRepeat.noRepeat),
                       ),
-                      alignment: Alignment.bottomCenter,
-                      child: const Text('Test')),
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'FINE LINE',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                          fontFamily: 'Ubuntu',
+                          fontSize: 20,
+                          shadows: [
+                            Shadow(
+                              blurRadius: 10.0,
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              offset: Offset(3.0, 3.0),
+                            ),
+                          ],
+                        ),
+                      )),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Container(
-                      width: MediaQuery.of(context).size.width,
                       decoration: BoxDecoration(
-                        color: Colors.yellow,
                         borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/img_cat2.png'),
+                            fit: BoxFit.cover,
+                            alignment: Alignment.center,
+                            repeat: ImageRepeat.noRepeat),
                       ),
-                      alignment: Alignment.bottomCenter,
-                      child: const Text('Test')),
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'GEEK',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                          fontFamily: 'Ubuntu',
+                          fontSize: 20,
+                          shadows: [
+                            Shadow(
+                              blurRadius: 10.0,
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              offset: Offset(3.0, 3.0),
+                            ),
+                          ],
+                        ),
+                      )),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Container(
-                      width: MediaQuery.of(context).size.width,
                       decoration: BoxDecoration(
-                        color: Colors.yellow,
                         borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/img_cat3.png'),
+                            fit: BoxFit.cover,
+                            alignment: Alignment.center,
+                            repeat: ImageRepeat.noRepeat),
                       ),
-                      alignment: Alignment.bottomCenter,
-                      child: const Text('Test')),
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'OLD SCHOOL',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                          fontFamily: 'Ubuntu',
+                          fontSize: 20,
+                          shadows: [
+                            Shadow(
+                              blurRadius: 10.0,
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              offset: Offset(3.0, 3.0),
+                            ),
+                          ],
+                        ),
+                      )),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Container(
-                      width: MediaQuery.of(context).size.width,
                       decoration: BoxDecoration(
-                        color: Colors.yellow,
                         borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/img_cat4.png'),
+                            fit: BoxFit.cover,
+                            alignment: Alignment.center,
+                            repeat: ImageRepeat.noRepeat),
                       ),
-                      alignment: Alignment.bottomCenter,
-                      child: const Text('Test')),
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'NEW SCHOOL',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                          fontFamily: 'Ubuntu',
+                          fontSize: 20,
+                          shadows: [
+                            Shadow(
+                              blurRadius: 10.0,
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              offset: Offset(3.0, 3.0),
+                            ),
+                          ],
+                        ),
+                      )),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Container(
-                      width: MediaQuery.of(context).size.width,
                       decoration: BoxDecoration(
-                        color: Colors.yellow,
                         borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/img_cat5.png'),
+                            fit: BoxFit.cover,
+                            alignment: Alignment.center,
+                            repeat: ImageRepeat.noRepeat),
                       ),
-                      alignment: Alignment.bottomCenter,
-                      child: const Text('Test')),
+                      alignment: Alignment.center,
+                      child: Text(
+                        'BLACKWORK',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                          fontFamily: 'Ubuntu',
+                          fontSize: 20,
+                          shadows: [
+                            Shadow(
+                              blurRadius: 10.0,
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              offset: Offset(3.0, 3.0),
+                            ),
+                          ],
+                        ),
+                      )),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Container(
-                      width: MediaQuery.of(context).size.width,
                       decoration: BoxDecoration(
-                        color: Colors.yellow,
                         borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/img_cat6.png'),
+                            fit: BoxFit.cover,
+                            alignment: Alignment.center,
+                            repeat: ImageRepeat.noRepeat),
                       ),
-                      alignment: Alignment.bottomCenter,
-                      child: const Text('Test')),
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'REALISMO',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                          fontFamily: 'Ubuntu',
+                          fontSize: 20,
+                          shadows: [
+                            Shadow(
+                              blurRadius: 10.0,
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              offset: Offset(3.0, 3.0),
+                            ),
+                          ],
+                        ),
+                      )),
                 ),
               ]),
         ]));
