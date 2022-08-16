4e7f75e1609894215fa062ff3e08973608ce4eb0
wheeout <xmomoon@gmail.com>
2022-08-15 22:56:52 -0300

    Criação da área de "Encontre sua inspiração"

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -8,36 +8,110 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height * 0.4,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(0),
-              topRight: Radius.circular(0),
-              bottomLeft: Radius.circular(20),
-              bottomRight: Radius.circular(20),
+        backgroundColor: Color.fromARGB(0, 38, 38, 38),
+        body: ListView(children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.4,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(0),
+                topRight: Radius.circular(0),
+                bottomLeft: Radius.circular(25),
+                bottomRight: Radius.circular(25),
+              ),
+              image: DecorationImage(
+                  image: AssetImage('assets/images/img_main.jpeg'),
+                  fit: BoxFit.cover,
+                  alignment: Alignment.center,
+                  repeat: ImageRepeat.noRepeat),
             ),
-            image: DecorationImage(
-                image: AssetImage('assets/images/img_main.jpeg'),
-                fit: BoxFit.cover,
-                alignment: Alignment.center,
-                repeat: ImageRepeat.noRepeat),
           ),
-        ),
-        // GridView.count(crossAxisCount: 2, children: [
-        //   Container(
-        //     width: MediaQuery.of(context).size.width,
-        //     height: MediaQuery.of(context).size.height * 0.4,
-        //     decoration: BoxDecoration(
-        //       color: Colors.yellow,
-        //     ),
-        //     alignment: Alignment.bottomCenter,
-        //     child: const Text('Test'),
-        //   )
-        // ])
-      ]),
-    );
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 40, 10, 10),
+            child: Container(
+                width: MediaQuery.of(context).size.width,
+                alignment: Alignment.bottomCenter,
+                child: const Text('✨ ENCONTRE INSPIRAÇÃO ✨',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20))),
+          ),
+          GridView.count(
+              crossAxisCount: 2,
+              childAspectRatio: MediaQuery.of(context).size.width /
+                  (MediaQuery.of(context).size.height / 4),
+              shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      alignment: Alignment.bottomCenter,
+                      child: const Text('Test')),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      alignment: Alignment.bottomCenter,
+                      child: const Text('Test')),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      alignment: Alignment.bottomCenter,
+                      child: const Text('Test')),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      alignment: Alignment.bottomCenter,
+                      child: const Text('Test')),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      alignment: Alignment.bottomCenter,
+                      child: const Text('Test')),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      alignment: Alignment.bottomCenter,
+                      child: const Text('Test')),
+                ),
+              ]),
+        ]));
   }
 }
